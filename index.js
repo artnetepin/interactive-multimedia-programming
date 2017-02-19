41845d2939287d66589f9454e1421c58375ea8c4
Artem Netepin <artnetepin@gmail.com>
2017-02-19 13:15:50 +0200

    HTML pages created

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ var app = express();
 var PORT = process.env.PORT || 5000;
 app.set('port', PORT);
 
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
 // Start to listen incomig requests (start the web server)
 app.listen(app.get('port'), function() {
   console.log('Up and running in a port:', PORT);
